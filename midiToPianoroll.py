18554e4da76c22b863cd852fd73303929cc91e29
Josh Grossman <jdgg@soal-2.stanford.edu>
2020-05-27 18:43:57 -0700

    bakcing up

--- midiToPianoroll.py
+++ midiToPianoroll.py
@@ -6,6 +6,7 @@ import pretty_midi
 import librosa
 import constants
 import csv
+import pdb
 
 # library functions:
 # pretty_midi.get_piano_roll(fs=100, times=None)
@@ -39,82 +40,161 @@ def get_piano_roll_subdivided_by_ms(midi_path, sampling_rate=constants.SAMPLE_RA
     return np.asarray(piano_roll[19:-21]).astype(int) # TODO: look at piano check to make sure we're removing the 
                                     # right one (not flipped top/bottom)... else [21:-19]
 
-### LOOK AT THIS
-def format_piano_roll_for_midi(piano_roll_path):
-    """
-    Takes in path to piano roll csv and reads csv into numpy array. Adds 19 rows pertaining 
-    to the highest values and 21 rows pertaining to the lowest values to the piano roll.
-    """
-    # NOTE: This is used to prepare a pianoroll matrix for conversion back into a midi, for listening.
-    small_piano_roll = np.genfromtxt(piano_roll_path, delimiter='\t')
-    piano_roll_length = small_piano_roll.shape[1]
-    piano_roll = np.insert(small_piano_roll, 0, np.zeros((19,piano_roll_length)), axis=0)
-    piano_roll = np.insert(piano_roll, piano_roll.shape[0], np.zeros((21,piano_roll_length)), axis=0)
-    return piano_roll.astype(int)
-
 def get_test_piano_roll(midi_filename):
     """
     Returns a np array that is a golden piano roll representation of a midi, divided into 1/87s slices
     """
     return get_piano_roll_subdivided_by_ms(midi_filename, constants.CQT_SAMPLE_RATE)
 
-
-## LOOK AT THIS
-def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0): 
-    """
-    Convert a Piano Roll array into a PrettyMidi object
-    with a single instrument.
-
-    From https://github.com/craffel/pretty-midi/blob/master/examples/reverse_pianoroll.py
-
-    Parameters
-    ----------
-    piano_roll : np.ndarray, shape=(128,frames), dtype=int
-        Piano roll of one instrument
-    fs : int
-        Sampling frequency of the columns, i.e. each column is spaced apart
-        by ``1./fs`` seconds.
-    program : int
-        The program number of the instrument.
-    Returns
-    -------
-    midi_object : pretty_midi.PrettyMIDI
-        A pretty_midi.PrettyMIDI class instance describing
-        the piano roll.
-    """
-    notes, frames = piano_roll.shape
-    pm = pretty_midi.PrettyMIDI()
-    instrument = pretty_midi.Instrument(program=program)
-
-    # pad 1 column of zeros so we can acknowledge inital and ending events
-    piano_roll = np.pad(piano_roll, [(0, 0), (1, 1)], 'constant')
-
-    # use changes in velocities to find note on / note off events
-    velocity_changes = np.nonzero(np.diff(piano_roll).T)
-
-    # keep track on velocities and note on times
-    prev_velocities = np.zeros(notes, dtype=int)
-    note_on_time = np.zeros(notes)
-
-    for time, note in zip(*velocity_changes):
-        # use time + 1 because of padding above
-        velocity = piano_roll[note, time + 1]
-        time = time / fs
-        if velocity > 0:
-            if prev_velocities[note] == 0:
-                note_on_time[note] = time
-                prev_velocities[note] = velocity
-        else:
-            pm_note = pretty_midi.Note(
-                velocity=prev_velocities[note],
-                pitch=note,
-                start=note_on_time[note],
-                end=time)
-            instrument.notes.append(pm_note)
-            prev_velocities[note] = 0
-    pm.instruments.append(instrument)
-    return pm
-
-
 def writePianorollToFile(pianoroll, path):
     np.savetxt(path, pianoroll, fmt='%i', delimiter='\t')
+    
+def convert_midi_to_pianoroll(cqt_dir, midi_dir, pianoroll_dir):
+    # Convert all MIDI to pianoroll labeled the same as CQT slices
+    
+    # Construct dict of pieceID -> list of ms offsets
+    pianorollIndicesToSample = collections.defaultdict(list)
+    
+    for cqtSlicePath in os.listdir(cqt_dir):
+        
+        # ignore hidden files and non-csv
+        if (cqtSlicePath[0] == "." or cqtSlicePath[-4:] != ".csv" ):
+            continue
+            
+        cqtSlicePath = os.path.join(cqt_dir, cqtSlicePath).replace("\\", "/")
+        p = re.compile("\/(.+)\_([0-9]+)\.csv$")
+        file_match = p.search(cqtSlicePath)
+        try:
+            pieceID = file_match.group(1)
+            sliceTimeInMs = int(file_match.group(2))
+            pianorollIndicesToSample[pieceID].append(sliceTimeInMs)
+        except Exception as e:
+            print(e)
+            continue
+    
+    # Get the relevant pianoroll slices
+    for pieceID in pianorollIndicesToSample:
+        midiFilename = os.path.join(midi_dir, pieceID) + ".midi"
+        try:
+            pianoroll = midiToPianoroll.get_piano_roll_subdivided_by_ms(midiFilename)
+        except Exception as e:
+            print(e)
+            continue
+        for sliceTimeInMs in pianorollIndicesToSample[pieceID]:
+            pianorollSlice = midiToPianoroll.get_ground_truth_labels_at_time(pianoroll, sliceTimeInMs)
+            
+            # why is this here?
+            if type(pianorollSlice) == bool:
+                continue
+                
+            pianorollSlicePath = (os.path.join(pianoroll_dir, pieceID) + "_" + str(sliceTimeInMs) + ".csv").replace("\\", "/").replace("(", "").replace(")", "")
+            try:
+                midiToPianoroll.writePianorollToFile(pianorollSlice, pianorollSlicePath)
+            except Exception as e:
+                print(e)
+                continue
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = 'Converts a directory of MIDI to Pianorolls')
+    parser.add_argument('cqt', help='Directory of CQT Slices to Match')
+    parser.add_argument('midi', help='Input directory of MIDIs')
+    parser.add_argument('pianoroll', help='Output directory of Pianorolls')
+    args = parser.parse_args()
+    convert_midi_to_pianoroll(args.cqt, args.midi, args.pianoroll)
+
+    
+    
+# #### Export MIDI as audio
+
+#     # Generate MIDIs from pianoroll for human listening
+#     # for pieceID in pieceIDs:
+#     #     pianorollFilename = (constants.PIANOROLL_NAME_TEMPLATE % (pieceID, "")).replace("(", "").replace(")", "")
+#     #     outputPianorollPath = (constants.TEST_PIANOROLL_OUT_DIR + pianorollFilename).replace("\\", "/")
+#     #     midiPath = os.path.join(constants.TEST_MIDI_OUT_DIR, constants.MIDI_OUT_NAME_TEMPLATE % pieceID).replace("\\", "/")
+#     #     pianoroll = midiToPianoroll.format_piano_roll_for_midi(outputPianorollPath)
+#     #     prettyMidi = midiToPianoroll.piano_roll_to_pretty_midi(pianoroll, constants.CQT_SAMPLING_RATE)
+#     #     prettyMidi.write(midiPath)
+#     # print("MIDIs generated for human listening!\n" 
+
+# ### LOOK AT THIS
+# def format_piano_roll_for_midi(piano_roll_path):
+#     """
+#     Takes in path to piano roll csv and reads csv into numpy array. Adds 19 rows pertaining 
+#     to the highest values and 21 rows pertaining to the lowest values to the piano roll.
+#     """
+#     # NOTE: This is used to prepare a pianoroll matrix for conversion back into a midi, for listening.
+#     small_piano_roll = np.genfromtxt(piano_roll_path, delimiter='\t')
+#     piano_roll_length = small_piano_roll.shape[1]
+#     piano_roll = np.insert(small_piano_roll, 0, np.zeros((19,piano_roll_length)), axis=0)
+#     piano_roll = np.insert(piano_roll, piano_roll.shape[0], np.zeros((21,piano_roll_length)), axis=0)
+#     return piano_roll.astype(int)
+
+# ## LOOK AT THIS
+# def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0): 
+#     """
+#     Convert a Piano Roll array into a PrettyMidi object
+#     with a single instrument.
+
+#     From https://github.com/craffel/pretty-midi/blob/master/examples/reverse_pianoroll.py
+
+#     Parameters
+#     ----------
+#     piano_roll : np.ndarray, shape=(128,frames), dtype=int
+#         Piano roll of one instrument
+#     fs : int
+#         Sampling frequency of the columns, i.e. each column is spaced apart
+#         by ``1./fs`` seconds.
+#     program : int
+#         The program number of the instrument.
+#     Returns
+#     -------
+#     midi_object : pretty_midi.PrettyMIDI
+#         A pretty_midi.PrettyMIDI class instance describing
+#         the piano roll.
+#     """
+#     notes, frames = piano_roll.shape
+#     pm = pretty_midi.PrettyMIDI()
+#     instrument = pretty_midi.Instrument(program=program)
+
+#     # pad 1 column of zeros so we can acknowledge inital and ending events
+#     piano_roll = np.pad(piano_roll, [(0, 0), (1, 1)], 'constant')
+
+#     # use changes in velocities to find note on / note off events
+#     velocity_changes = np.nonzero(np.diff(piano_roll).T)
+
+#     # keep track on velocities and note on times
+#     prev_velocities = np.zeros(notes, dtype=int)
+#     note_on_time = np.zeros(notes)
+
+#     for time, note in zip(*velocity_changes):
+#         # use time + 1 because of padding above
+#         velocity = piano_roll[note, time + 1]
+#         time = time / fs
+#         if velocity > 0:
+#             if prev_velocities[note] == 0:
+#                 note_on_time[note] = time
+#                 prev_velocities[note] = velocity
+#         else:
+#             pm_note = pretty_midi.Note(
+#                 velocity=prev_velocities[note],
+#                 pitch=note,
+#                 start=note_on_time[note],
+#                 end=time)
+#             instrument.notes.append(pm_note)
+#             prev_velocities[note] = 0
+#     pm.instruments.append(instrument)
+#     return pm
+
+# #### Export MIDI as audio
+
+#     # Generate MIDIs from pianoroll for human listening
+#     # for pieceID in pieceIDs:
+#     #     pianorollFilename = (constants.PIANOROLL_NAME_TEMPLATE % (pieceID, "")).replace("(", "").replace(")", "")
+#     #     outputPianorollPath = (constants.TEST_PIANOROLL_OUT_DIR + pianorollFilename).replace("\\", "/")
+#     #     midiPath = os.path.join(constants.TEST_MIDI_OUT_DIR, constants.MIDI_OUT_NAME_TEMPLATE % pieceID).replace("\\", "/")
+#     #     pianoroll = midiToPianoroll.format_piano_roll_for_midi(outputPianorollPath)
+#     #     prettyMidi = midiToPianoroll.piano_roll_to_pretty_midi(pianoroll, constants.CQT_SAMPLING_RATE)
+#     #     prettyMidi.write(midiPath)
+#     # print("MIDIs generated for human listening!\n" 
---
+++ wav_to_cqt_slices.py
@@ -0,0 +1,77 @@
+import os.path
+import constants
+import librosa
+import librosa.display
+import numpy as np
+import pdb
+import argparse
+import re
+import math
+import os
+import multiprocessing as mp
+
+def convert_wav_to_cqt(wavPath):
+    """
+    Converts a WAV file to a CQT representation
+    """
+    y, sr = librosa.load(path=wavPath, sr=None, offset=0)
+    print("Loaded WAV " + wavPath)
+    piece_id = os.path.basename(wavPath).replace(".wav", "")
+    cqt = np.abs(librosa.cqt(y, sr=sr, n_bins=constants.CONTEXT_WINDOW_ROWS, 
+                           bins_per_octave=constants.BINS_PER_OCTAVE, 
+                           hop_length=constants.HOP_LENGTH, 
+                           filter_scale=constants.FILTER_SCALE))
+    return piece_id, cqt
+
+def save_cqt_slice(cqtSlice, cqtSliceDir, pieceID, millisecondsOffset):
+    """
+    Saves a CQT slice as a binary file.
+    """
+    slice_path = (cqtSliceDir + "/" + pieceID + "_" + str(millisecondsOffset) + ".bin")
+    cqtSlice.tofile(slice_path)
+    return
+
+def make_cqt_slices(wavPath, cqtSliceDir):
+    """
+    Converts a WAV file into a directory of CQT slices
+    """
+    piece_id, cqt = convert_wav_to_cqt(wavPath)
+    cqtSlicePaths = []
+    
+    tot_time = cqt.shape[1]
+    radius = constants.CQT_SLICE_RADIUS_IN_PIXELS
+    offset = constants.CQT_SLICE_OFFSET_IN_PIXELS
+    
+    print("Saving slices for " + os.path.basename(wavPath))
+    for t in range(radius, tot_time-offset-radius-1, 2*radius+1):
+        cqtSlice = cqt[:, (t+offset-radius):(t+offset+radius+1)]
+        millisecondsOffset = int(math.ceil(float(t) / constants.CQT_SAMPLING_RATE * 1000))
+        save_cqt_slice(cqtSlice, cqtSliceDir, piece_id, millisecondsOffset)
+
+    return 
+
+def convert_dir(wav_dir, cqt_dir):
+    """
+    Converts a directory of WAV files into a directory of CQT slices
+    """
+    wav_re = re.compile("\.wav$")
+    for wavFilename in os.listdir(wav_dir):
+        if not wav_re.search(wavFilename):
+            print("Invalid file: " + wavFilename)
+            continue
+        wavPath = os.path.join(wav_dir, wavFilename)
+        make_cqt_slices(wavPath, cqt_dir)
+    return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = 'Converts a directory of WAV to CQT Slices')
+    parser.add_argument('wav_dir', help='Input directory of WAV')
+    parser.add_argument('cqt_dir', help='Output directory of CQT Slices')
+    args = parser.parse_args()
+    
+    #with mp.Pool(int(mp.cpu_count() / 2)) as pool:
+    
+    convert_dir(args.wav_dir, args.cqt_dir)
+    
+    
+    
